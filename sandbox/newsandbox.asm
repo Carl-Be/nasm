763dc525e85694b01f18382299cde54cf1207b48
Carl-Be <carlbechie@carlbechie.com>
2020-08-04 00:02:46 -0700

    Good Job!

--- sandbox/newsandbox.asm
+++ sandbox/newsandbox.asm
@@ -1,5 +1,9 @@
 section .data
 
+	msg: db "7",10,37h,10 ; message to be displayed 
+	len: equ $-msg        ; length of message to be displayed
+
+section .bss 
 
 section .text
 
@@ -8,11 +12,13 @@ section .text
 _start:
 	nop
 ; Put your experiments between the two nops...
-	
-	mov eax, 07FFFFFFFh
-	inc eax
+
+	mov eax, 4       ; specify sys_write syscall
+	mov ebx, 1       ; specify file desctiptor 1: Standard Output
+	mov ecx, msg     ; message to be displayed
+	mov edx, len     ; length of message 
+	int 80h	         ; Make syscall to  output the text to stdout
 
 ; Put your experiments between the two nops...
 	nop
 
-section .bss 
